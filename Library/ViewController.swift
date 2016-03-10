11ed43e4aa02b701a221113efc2c505cc4d20c4a
Carlos Buitrago <Carlos@macbook-pro-de-carlos.local>
2016-03-10 09:20:48 -0500

    Initial Commit

---
+++ Library/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Library
+//
+//  Created by Carlos Buitrago on 10/03/16.
+//  Copyright © 2016 Carlos Buitrago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
