0c842bab9a97218e5f082b7725fe48e3199af515
Carlos Buitrago <Carlos@macbook-pro-de-carlos.local>
2016-03-10 11:21:45 -0500

    1

--- Library/ViewController.swift
+++ Library/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var texto: UITextView!
+    @IBOutlet weak var textoIn: UITextField!
+    
+    let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        textoIn.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func limpiar(sender: AnyObject) {
+        textoIn.text = ""
+        texto.text = ""
+    }
+    @IBAction func backGroundTap(sender:UIControl){
+        textoIn.resignFirstResponder()
+    }
+    @IBAction func textFieldDoneEditing(sender:UITextField){
+        sender.resignFirstResponder()
+        let urls2 = urls + textoIn.text!
+        let url = NSURL(string: urls2)
+        let datos:NSData? = NSData(contentsOfURL: url!)
+        if datos != nil{
+        let textoS = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+        texto.text = String(textoS)
+        }
+        else {
+            let alert = UIAlertController(title:"Error", message: "No hay conexion a Internet", preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            presentViewController(alert, animated: true, completion: nil)
 
+        }
+        
+    }
 
 }
 
