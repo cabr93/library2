89eb9ce14658860d6a4c0de2a42dd5050b11c36d
Carlos Buitrago <Carlos@macbook-pro-de-carlos.local>
2016-03-16 19:14:04 -0500

    1

--- Library/ViewController.swift
+++ Library/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var texto: UITextView!
     @IBOutlet weak var textoIn: UITextField!
-    @IBOutlet weak var boton: UIButton!
+    @IBOutlet var Control: UIView!
+    @IBOutlet weak var ima: UIImageView!
     
     let urls = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
     
@@ -20,9 +21,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         textoIn.delegate = self
-        boton.layer.cornerRadius = 10
-        boton.layer.masksToBounds = true
-        boton.backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.7)
+        Control.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        texto.backgroundColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        texto.font = UIFont.systemFontOfSize(22)
+        texto.textColor = UIColor(red: 1, green: 1 , blue: 1, alpha: 1)
+        texto.font = UIFont.boldSystemFontOfSize(20)
+        texto.editable = false
+        texto.layer.cornerRadius = 10
         
 
     }
@@ -31,27 +36,47 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBAction func limpiar(sender: AnyObject) {
-        textoIn.text = ""
-        texto.text = ""
-    }
     @IBAction func backGroundTap(sender:UIControl){
         textoIn.resignFirstResponder()
     }
     @IBAction func textFieldDoneEditing(sender:UITextField){
         sender.resignFirstResponder()
-        let urls2 = urls + textoIn.text!
+        let texIn = textoIn.text!
+        let urls2 = urls + texIn
         let url = NSURL(string: urls2)
-        let datos:NSData? = NSData(contentsOfURL: url!)
-        if datos != nil{
-        let textoS = NSString(data: datos!, encoding: NSUTF8StringEncoding)
-        texto.text = textoS as! String
-        }
-        else {
+        let datos = NSData(contentsOfURL: url!)
+        if datos == nil{
             let alert = UIAlertController(title:"Error", message: "No hay conexion a Internet", preferredStyle: .Alert)
             alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
             presentViewController(alert, animated: true, completion: nil)
+        }
+        else {
+            let textoS = NSString(data: datos!, encoding: NSUTF8StringEncoding)
+            if textoS as! String == "{}"{
+                let alert = UIAlertController(title:"Error", message: "El ISBN no existe", preferredStyle: .Alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+                presentViewController(alert, animated: true, completion: nil)
+            }
+            else{
+                do{
+                    let json = try NSJSONSerialization.JSONObjectWithData(datos!, options: NSJSONReadingOptions.MutableLeaves)
+                    let dico1 = json as! NSDictionary
+                    let dico2 = dico1["ISBN:"+texIn] as! NSDictionary
+                    let titulo = dico2["title"] as! NSString as String
+                    let todo:String = "Titulo:\n" + titulo
+                    let dico3 = dico2["cover"] as! NSDictionary
+                    let foto = dico3["large"]as! NSString as String
+                    let fot = NSURL(string: foto)
+                    let dat = NSData(contentsOfURL: fot!)
+                    ima.image = UIImage(data:dat!)
 
+                    
+                    texto.text = todo
+                }
+                catch _ {
+                    
+                }
+            }
         }
         
     }
